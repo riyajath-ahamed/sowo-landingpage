57cb2a6b9700031820b2ef98bd4056679bb5cfed
riyajath-ahamed <riyajatha@gmail.com>
2024-07-03 19:57:15 +0530

    Vision | SOWO

--- src/components/vision.jsx
+++ src/components/vision.jsx
@@ -30,17 +30,6 @@ const Vision = () => {
                 </h3>
               Empowering society through Social Works
               </p>
-              <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-                <a
-                  href="#"
-                  className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-                >
-                  Get started
-                </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-white">
-                  Learn more <span aria-hidden="true">→</span>
-                </a>
-              </div>
             </div>
           </div>
         </div>
